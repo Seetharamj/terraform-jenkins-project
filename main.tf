2f3449cc94b17f2e674805572abe145084ae29f0
seetharam j <seetharamj704@gmail.com>
2025-07-03 10:04:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = var.aws_key_pair
+  public_key = var.aws_public_key
+}
+
+
+resource "aws_security_group" "ssh_access" {
+  name        = "allow-ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Be cautious in production
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SSHAccess"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+   key_name                    = aws_key_pair.deployer.key_name
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [aws_security_group.ssh_access.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Linux-webserver"
+  }
+}
+
+resource "aws_ami_from_instance" "ami" {
+  name               = "my-ami"
+  source_instance_id = aws_instance.web.id
+
+tags = {
+    Name ="Linux-ami"
+   }
+}
