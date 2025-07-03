52e034560907fea27e3fd9e1384f8ab95fd91b3b
seetharam j <seetharamj704@gmail.com>
2025-07-03 12:19:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,17 +16,16 @@ resource "aws_key_pair" "deployer" {
   public_key = var.aws_public_key
 }
 
-
 resource "aws_security_group" "ssh_access" {
   name        = "allow-ssh"
   description = "Allow SSH inbound traffic"
-  vpc_id      = var.vpc_id
+  vpc_id      = var.vpc_id  # ✅ Make sure this is a valid VPC ID in terraform.tfvars
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Be cautious in production
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -42,10 +41,10 @@ resource "aws_security_group" "ssh_access" {
 }
 
 resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-   key_name                    = aws_key_pair.deployer.key_name
-  subnet_id                   = var.subnet_id
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.deployer.key_name
+  subnet_id                   = var.subnet_id  # ✅ Must be a valid subnet in the same VPC
   vpc_security_group_ids      = [aws_security_group.ssh_access.id]
   associate_public_ip_address = true
 
@@ -58,7 +57,7 @@ resource "aws_ami_from_instance" "ami" {
   name               = "my-ami"
   source_instance_id = aws_instance.web.id
 
-tags = {
-    Name ="Linux-ami"
-   }
+  tags = {
+    Name = "Linux-ami"
+  }
 }
